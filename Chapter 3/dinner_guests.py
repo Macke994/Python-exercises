5755bef5f7051812147c667f7e4ce1917621693c
Macke994 <marcus.hansson@edunord.se>
2025-11-11 15:24:15 +0100

    Add files via upload

---
+++ Chapter 3/dinner_guests.py
@@ -0,0 +1,7 @@
+names = ['Linus', 'Stefan', 'Mari',]
+
+print(f"{names[0]} I welcome you to dinner.")
+print(f"{names[1]} I welcome you to dinner.")
+print(f"{names[2]} I welcome you to dinner.")
+
+print(f"There are {len(names)} people invited to dinner.")
---
+++ Chapter 3/seeing_the_world.py
@@ -0,0 +1,20 @@
+places = ['Japan', 'America', 'Africa', 'Cuba', 'Canary Islands']
+print(places)
+
+print(sorted(places))
+print(places)
+
+print(sorted(places, reverse=True))
+print(places)
+
+places.reverse()
+print(places)
+
+places.reverse()
+print(places)
+
+places.sort()
+print(places)
+
+places.sort(reverse=True)
+print(places)
