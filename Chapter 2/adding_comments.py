b91ff5f9fa4bef0bf56db48fabe01957167d5076
Macke994 <marcus.hansson@edunord.se>
2025-11-11 13:52:45 +0100

    Add files via upload

---
+++ Chapter 2/adding_comments.py
@@ -0,0 +1,10 @@
+# Printing out numbers using four different calculations.
+print(5+3)
+print(12-4)
+print(4*2)
+print(16 // 2)
+
+# Writing out my favorite number in a sentence.
+x, = 10,
+message = f"My favorite number is {x}"
+print(message)
