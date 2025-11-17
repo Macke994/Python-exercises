5a3bb3b0dc555ab1f56dfa3792a52eb439261cec
Macke994 <marcus.hansson@edunord.se>
2025-11-17 14:06:40 +0100

    Add files via upload

---
+++ Chapter 4/buffet.py
@@ -0,0 +1,11 @@
+foods = ('pizza', 'pasta', 'meatloaf', 'rice', 'chicken')
+
+print("Food we serve here:")
+for food in foods:
+    print(food.title())
+
+foods = ('pie', 'pasta', 'lasagna', 'rice', 'chicken')
+
+print("\nRan out of some so heres a new list:")
+for food in foods:
+    print(food.title())
