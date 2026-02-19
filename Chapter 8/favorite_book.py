264f18fb53259c699992cb5b3fd9c48ff63c3b33
Macke994 <marcus.hansson@edunord.se>
2026-02-19 09:36:35 +0100

    Add files via upload

---
+++ Chapter 8/favorite_book.py
@@ -0,0 +1,5 @@
+def favorite_book(booktitle):
+    """Display a book title"""
+    print(f"My favorite book is, {booktitle.title()}")
+
+favorite_book("The meg")
---
+++ Chapter 8/message.py
@@ -0,0 +1,5 @@
+def display_message():
+    """Display a message"""
+    print("Im learning about functions")
+
+display_message()
